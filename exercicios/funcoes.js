b54d47dbdd718806ffa1fb6fedc3fa7877273713
Gregory Klaus <gregory.klaus2015@gmail.com>
2024-09-30 23:48:54 -0300

    Add files via upload

---
+++ exercicios/funcoes.js
@@ -0,0 +1,122 @@
+
+function exercicio001() {
+        // Lê um valor do usuário
+        let numero = prompt("Digite um número:");
+    
+        // Converte o valor digitado para número
+        numero = Number(numero);
+    
+        // Calcula o antecessor
+        let antecessor = numero - 1;
+    
+        // Exibe o antecessor
+        alert("O antecessor de " + numero + " é " + antecessor);
+    
+    
+    // Chamando a função
+    mostrarAntecessor();
+    
+}
+function exercicio002() {
+    // Lê um valor do usuariio
+    let numero = prompt("Digite um número:");
+    // Convertando o valor digitado para número
+    numero = Number(numero);
+    // Calcula o sucessor
+    let sucessor = numero + 1;
+    // Exibe o sucessor
+    alert(`O sucessor de ${numero} é ${sucessor}`)
+
+}
+function exercicio003() {
+    //Entradade de dados do usuarios
+    let numero = Number(prompt("Digite o primeiro numero:"))
+    let numero2 = Number(prompt("Digite o segundo numero:"))
+    // Calculando a soma
+    let soma = numero + numero2
+    // Exibindo o resultado para o usuario
+    alert(`O resultaado da some de ${numero}+${numero2}: ${soma}`)
+    
+}
+function exercicio004() {}
+function exercicio005() {}
+function exercicio006() {}
+function exercicio007() {}
+function exercicio008() {}
+function exercicio009() {}
+function exercicio010() {}
+function exercicio011() {}
+function exercicio012() {}
+function exercicio013() {}
+function exercicio014() {}
+function exercicio015() {}
+function exercicio016() {}
+function exercicio017() {}
+function exercicio018() {}
+function exercicio019() {}
+function exercicio020() {}
+function exercicio021() {}
+function exercicio022() {}
+function exercicio023() {}
+function exercicio024() {}
+function exercicio025() {}
+function exercicio026() {}
+function exercicio027() {}
+function exercicio028() {}
+function exercicio029() {}
+function exercicio030() {}
+function exercicio031() {}
+function exercicio032() {}
+function exercicio033() {}
+function exercicio034() {}
+function exercicio035() {}
+function exercicio036() {}
+function exercicio037() {}
+function exercicio038() {}
+function exercicio039() {}
+function exercicio040() {}
+function exercicio041() {}
+function exercicio042() {}
+function exercicio043() {}
+function exercicio044() {}
+function exercicio045() {}
+function exercicio046() {}
+function exercicio047() {}
+function exercicio048() {}
+function exercicio049() {}
+function exercicio050() {}
+function exercicio051() {}
+function exercicio052() {}
+function exercicio053() {}
+function exercicio054() {}
+function exercicio055() {}
+function exercicio056() {}
+function exercicio057() {}
+function exercicio058() {}
+function exercicio059() {}
+function exercicio060() {}
+function exercicio061() {}
+function exercicio062() {}
+function exercicio063() {}
+function exercicio064() {}
+function exercicio065() {}
+function exercicio066() {}
+function exercicio067() {}
+function exercicio068() {}
+function exercicio069() {}
+function exercicio070() {}
+function exercicio071() {}
+function exercicio072() {}
+function exercicio073() {}
+function exercicio074() {}
+function exercicio075() {}
+function exercicio076() {}
+function exercicio077() {}
+function exercicio078() {}
+function exercicio079() {}
+function exercicio080() {}
+function exercicio081() {}
+function exercicio082() {}
+function exercicio083() {}
+function exercicio084() {}
+function exercicio085() {}
